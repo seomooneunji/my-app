a8ca2fb1e1154dc1af8f62dcbc8c430070616db2
eunji <smej201@naver.com>
2018-11-21 23:13:49 +0900

    smart component VS dumb component
    클래스 컴포넌트를 functional 컴포넌트로 고쳐봄. 펑셔녈은 state를 못가지기때문에 state의 라이플 사이클을 가지지 못하여 데이터를 업데이트하거나 이런 행동들에 제약이 생김.
    
    그렇지만 예제로 만드려는 영화정보 보여주는 사이트에서는 무방함.

--- src/Movie.js
+++ src/Movie.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './Movie.css';
 import PropTypes from 'prop-types'
 
+/*
 class Movie extends Component{
 
     // 부모 컴포넌트에서 넘어오는 값의 형태가 string인지 아닌지를 체크할수이뜸.
@@ -24,9 +25,20 @@ class Movie extends Component{
             </div>
         )
     }
+}
+*/
 
+// 위의 클래스 컴포넌트를 functional 컴포넌트로 바꿔보자~!
+function Movie({title, poster}){
+    return (
+        <div>
+            <MoviePoster poster={poster}/>            
+            <h1>{title}</h1>
+        </div>
+    )
 }
 
+/*
 class MoviePoster extends Component{
     static propTypes = {
         poster: PropTypes.string.isRequired,
@@ -35,22 +47,42 @@ class MoviePoster extends Component{
     render(){
         //console.log(this.props)
         return(
-            <img src={this.props.poster} />
+            <img src={this.props.poster} alt="Movie Pster" />
         )
     }
 }
+*/
 
+function MoviePoster({poster}){
+    return (
+        <img src={poster} alt="Movie Pster" />  
+    )
+}
+// 이 function컴포넌트는 클래스 컴포넌트가 아니기때문에, props는 지우고, poster만 남김.
+// 어떤 컴포넌트는 그냥 return을 하기 위해 존재함. 그냥 1개의 props, 1개의 html태그만 있으면됨.
+
+Movie.protoType = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+}
+
+MoviePoster.protoType = {
+    poster: PropTypes.string.isRequired
+}
 
 export default Movie
 
 /*
 리액트에는 2개의 주요 컨셉 - state, props
 
-props - 
-
-메인 컴포넌트 App은 모든 영화의 리스트가 있음. 
+props - 메인 컴포넌트 App은 모든 영화의 리스트가 있음. 
 그 리스트 안의 영화카드엔 해당 영화의 정보가 각각 담길것임.
 즉, 부모 컴포넌트는 칠드런 컴포넌트에게 각각 정보를 준다는 뜻임.
 어떻게 부모가 자식에게 정보를 주느냐? props를 통해서임.
+
+state - 리액트 컴포넌트 안에 있는 오브젝트임.
+state가 바뀔때마다, 컴포넌트는 다시 render 함.
+
+어떤 컴포넌트는 state가 없는 statelss functional 컴포넌트임. - dump vs smart
 
 */
